1d0f4965f3a271d831d496863566d6cf09363879
minefarmer <pgoldfarm@yahoo.com>
2020-04-20 22:19:24 -0500

    Need to think about PyCharm

---
+++ Course Notes/User_defined functions.py
@@ -0,0 +1,11 @@
+# def my_function(): 
+# 	print("This is my function!")  # This is my function!
+# 	print("A second string.")  # A second string.
+	
+# my_function()  # this line needed before Python will print the function
+
+def my_function(str1, str2):
+    print(str1)
+    print(str2)
+
+    my_function("This is argument 1", "This is also a string.")
