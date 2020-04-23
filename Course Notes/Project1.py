a9ef5277c8308d866e5041a360905656bd3833ef
minefarmer <pgoldfarm@yahoo.com>
2020-04-22 21:15:58 -0500

    "Course Notes: Quiting for the Night

---
+++ Course Notes/Project1.py
@@ -0,0 +1,27 @@
+import re
+
+print("Our Magical Caculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = input("Enter equation:")
+    if previous == 0:
+        equation = input("enter equation:")
+    else:
+        equation = re.sub('[a-zA=Z,.:()" "]', '', equation)
+
+    if previous == 0:
+        previous == eval(equation)
+    else:
+        previous = eval(str(previous) + equation)
+
+
+        print("You typed", previous)
+
+while run:
+    performMath()
