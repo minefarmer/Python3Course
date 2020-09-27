438d2558cac57364eb4e41a27bfb558a160ac0f9
minefarmer <pgoldfarm@yahoo.com>
2020-09-27 18:38:58 -0500

    stopping the course

---
+++ ClassFiles/PepGuidlines/main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello World")
+
+
+def second_function():
+    print("Second function")
+    
+    
+my_list = [1, 2,
+           3, 4,
+           5, 6]
+
+
+print("Hello ", "HI")
+
---
+++ ClassFiles/ProgrammingBasics/WhileLoops.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+playerhp = 260
+enemyatkl = 60
+enemyatkh = 80
+
+while playerhp > 0:
+    dmg = random.randrange(enemyatkl, enemyatkh)
+    
